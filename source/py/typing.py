21b3f3c411177c00581b68d02eddc069f86be03b
Philip May <eniak.info@gmail.com>
2021-07-08 10:52:47 +0200

    Update typing.py

--- source/py/typing.py
+++ source/py/typing.py
@@ -1,7 +1,11 @@
 # Typing
 - https://docs.python.org/3/library/typing.html
-  
-  ## Examples
-  - dict
-    - Dict[str, int]
-    - Dict[str, List[int]]
+
+## Examples
+- `Any`: Special type indicating an unconstrained type.  
+- `Optional`: Optional type.
+- Dict
+  - Dict[str, int]
+  - Dict[str, List[int]]
+- Callable
+  - Callable[[str], Dict[str, List[int]]]
